51f234a5654b488a1d736c054faf9f93d1c2419d
TL <TL@gmail.com>
2020-04-25 17:17:31 +0530

    personal+utility class updated

--- DemoProject/src/com/gotam/test/Utility.java
+++ DemoProject/src/com/gotam/test/Utility.java
@@ -5,6 +5,7 @@ class Utility {
 	public static void main(String[] args) {
 		System.out.println("hello world 123");
 		System.out.println("hello world 456");
+		System.out.println("hello world 456777777");
 	}
 
 }
